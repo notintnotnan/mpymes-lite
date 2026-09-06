9bcc5b54075fce73ba753236eeff8295abf0e7da
Oscar Ramirez <oscarframirezp@hotmail.com>
2026-09-06 17:35:47 -0500

    Corrected db temp sqlite storage path and added specific dir in project.

--- app/core/config.py
+++ app/core/config.py
@@ -1,22 +1,32 @@
+from typing import Any, Optional
 from dotenv import load_dotenv
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings
 
 load_dotenv()
 
 class Config(BaseSettings):
-    fastapi_app_name:str
+    fastapi_app_name:str = "mpymes lite"
     debug:bool = False
     database_username:str
     database_password:str
     database_name:str
     database_host:str = None
-    database_port:int = None
+    database_port:Optional[int] = None
 
     @property
     def database_url(self):
         if self.database_host:
             return f"postgresql://{self.database_username}:{self.database_password}@{self.database_host}:{self.database_port}/{self.database_name}?sslmode=require"
         else:
-            return f"sqlite:///./{self.database_name}"
+            return f"sqlite:///./temp/{self.database_name}"
+
+    @field_validator("database_port", mode="before")
+    @classmethod
+    def assemble_database_port(cls, value:Any) -> Any:
+        if value == "":
+            return None
+        return value
 
 config = Config()
--- app/db/session.py
+++ app/db/session.py
@@ -4,4 +4,4 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import config
 
 engine = create_engine(config.database_url, connect_args={"check_same_thread":False})
-databse_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+database_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
